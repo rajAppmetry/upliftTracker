e5f292881a7b6d9f4b89e330c2d5a129e9fec3aa
Rajasekar <rsekar1094@gmail.com>
2024-11-24 19:35:43 +0530

    Update the tracker

--- Package.swift
+++ Package.swift
@@ -14,10 +14,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "UpliftTracker",
-            publicHeadersPath: "include"
-        ),
-
+        .binaryTarget(
+                    name: "UpliftTracker",
+                    path: "./Tracker.xcframework"
+                )
     ]
 )
