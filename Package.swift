3c22c8602ce878b38148cad31f64355758a356ee
Rajasekar <rsekar1094@gmail.com>
2024-11-24 20:13:51 +0530

    update tracker

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "UpliftTracker",
+    name: "UpliftTrackerSPM",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "UpliftTracker",
+            name: "UpliftTrackerSPM",
             targets: ["UpliftTracker"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
-                    name: "UpliftTracker",
-                    path: "./Tracker.xcframework"
-                )
+            name: "UpliftTracker",
+            path: "./Tracker.xcframework"
+        )
     ]
 )
