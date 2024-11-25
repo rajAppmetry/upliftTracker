84bcfd37257c7d2124dc61ac1c05cf9786ec91e4
Rajasekar <rsekar1094@gmail.com>
2024-11-25 07:54:16 +0530

    example app integration

---
+++ UpliftTrackerExample/UpliftTrackerExample/UpliftTrackerExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  UpliftTrackerExampleApp.swift
+//  UpliftTrackerExample
+//
+//  Created by Raj S on 24/11/24.
+//
+
+import SwiftUI
+import UpliftTrackerSPM
+
+@main
+struct UpliftTrackerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
